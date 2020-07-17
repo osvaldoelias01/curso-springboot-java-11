cf4798c63381f4b22f3d07b3c4245bbbf4b57165
Osvaldo Elias <osvaldom.elias@hotmail.com>
2020-07-17 03:09:10 +0100

    H2 database, test profile, JPA

--- src/main/java/com/example/cursospringboot/entities/User.java
+++ src/main/java/com/example/cursospringboot/entities/User.java
@@ -1,10 +1,18 @@
 package com.example.cursospringboot.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -21,6 +29,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    @Id
     public Long getId() {
         return id;
     }
